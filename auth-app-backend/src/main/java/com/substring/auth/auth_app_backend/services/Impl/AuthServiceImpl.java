89b149b079fd6be126a36f0631582ae51246e988
Rajeev  Singh <rajeevsinghece49@gmail.com>
2025-12-13 00:09:07 +0530

    Register User API added

---
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/services/Impl/AuthServiceImpl.java
@@ -0,0 +1,22 @@
+package com.substring.auth.auth_app_backend.services.Impl;
+
+import com.substring.auth.auth_app_backend.dtos.UserDto;
+import com.substring.auth.auth_app_backend.services.AuthService;
+import com.substring.auth.auth_app_backend.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final UserService userService;
+
+
+    @Override
+    public UserDto registerUser(UserDto userDto) {
+
+        return userService.createUser(userDto);
+    }
+}
